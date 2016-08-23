b1755726a0909c0230b67d62a667aeb5d9d7aa84
Oswaldo Morales Rodriguez <A01378566@itesm.mx>
2016-08-23 08:57:37 -0500

    Update Polar.py

--- Polar.py
+++ Polar.py
@@ -8,5 +8,6 @@ from math import atan2, pi
 x=int(input("valor de x"))
 y=int(input("valor de y"))
 r=((x**2)+(y**2))**0.5
-ang=atan2(y,x)
+z=atan2(y,x)
+ang=(180*z)/pi
 print("Hipotenusa es",r,"angulo es",ang)
